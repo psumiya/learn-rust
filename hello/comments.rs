ee25c1a0b61dca7405dcf9a0b01cd1738442cda7
psumiya <sumiya.p@gmail.com>
2023-05-20 18:50:30 -0700

    Comments

---
+++ hello/comments.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // Line Comment
+
+    /* Enclosed Comment - Example 1 */
+
+    /*
+     *
+     * Enclosed Comment - Example 2
+     *
+     */
+
+     /* Enclosed Comment - Example 3 */
+
+}
