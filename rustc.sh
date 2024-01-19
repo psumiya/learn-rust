423bc5f7c46e2deac2dd2899a99cef173e2a0ee6
psumiya <sumiya.p@gmail.com>
2024-01-19 00:44:33 -0800

    Add script - generated from perplexity

--- rustc.sh
+++ rustc.sh
@@ -1,10 +1,21 @@
-binaryName=awk '{print substr($0, 1, length($0)-3)}'
-echo binaryName
-rustc $0
-if [ $? -eq 0 ]; then
-    mv binaryName bin/binaryName
-else
-    echo "rustc failed."
+# Check if the Rust file name is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please provide the Rust file name as an argument"
+    exit 1
 fi
 
+# Extract the file name and extension
+file_name=$(basename "$1")
+file_name_no_ext=${file_name%.*}
 
+# Compile the Rust file
+rustc "$1"
+if [ $? -eq 0 ]; then
+    # Create the bin directory if it doesn't exist
+    mkdir -p bin
+    # Move the resulting binary to the bin directory
+    mv "$file_name_no_ext" "bin/"
+    echo "Compilation successful. Binary is located in the bin directory."
+else
+    echo "Compilation failed"
+fi
