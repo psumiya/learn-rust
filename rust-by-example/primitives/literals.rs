30e260bd7ac1f179222b4a3b5eb600b9aad3affd
psumiya <sumiya.p@gmail.com>
2024-01-18 22:53:00 -0800

    Add more

---
+++ rust-by-example/primitives/literals.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("1 - 2 = {}", 1 - 2);
+
+    println!("true && false = {}", true && false);
+
+    println!("00 & 01 = {:02b}", 0b00 & 0b01);
+
+    println!("00 | 01 = {:02b}", 0b00 | 0b01);
+}
