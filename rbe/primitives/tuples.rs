616501978e5b32781ebdbb9ba4494801f27bc55b
psumiya <sumiya.p@gmail.com>
2024-01-24 22:30:14 -0800

    Activity - Formatted display

--- rbe/primitives/tuples.rs
+++ rbe/primitives/tuples.rs
@@ -6,11 +6,19 @@ fn reverse(tuple: (i32, bool)) -> (bool, i32) {
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
+use std::fmt;
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} ) \n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
 fn main() {
   let x = reverse((5, true));
   println!("{:?}", x);
 
   let y = Matrix(1.1, 2.2, 3.3, 4.4);
   println!("{:?}", y);
+  println!("{}", y);
 }
 
